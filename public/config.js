d0e657d914e35bcae0530676d59595e64e914973
Mario Reder <mario.reder@pm.me>
2025-11-06 16:25:39 +0100

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "b2z-dex",
   "VITE_ORDERLY_BROKER_NAME": "B2Z",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
